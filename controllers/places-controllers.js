7b7cacc060e06afc48ba8d621e5be3db1ca10107
FACTS  LIMITED <admin@factsafrica.com>
2020-06-13 10:59:54 +0300

    add a POST route

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const HttpError = require('../models/http-error');
+const uuid = require('uuid/v4')
 
 const DUMMY_PLACES = [
   {
@@ -54,5 +55,23 @@ const getPlaceByUser = (req, res, next) => {
   res.json({ places });
 };
 
-exports.getPlaceById = getPlaceById
-exports.getPlaceByUser = getPlaceByUser
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuid(),  
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUser = getPlaceByUser;
+exports.createPlace = createPlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -8,4 +8,6 @@ router.get('/:placeId', placesControllers.getPlaceById );
 
 router.get('/user/:userId', placesControllers.getPlaceByUser);
 
+router.post('/', placesControllers.createPlace)
+
 module.exports = router;
