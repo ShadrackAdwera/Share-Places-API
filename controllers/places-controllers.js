c0c061516e15e32bea117d46b15fd999e54b2f6c
FACTS  LIMITED <admin@factsafrica.com>
2020-06-15 18:58:37 +0300

    hash the user password

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const mongoose = require('mongoose');
 const { validationResult } = require('express-validator');
 const getCoordsFromAddress = require('../utils/location');
@@ -182,6 +183,8 @@ const deletePlace = async (req, res, next) => {
     return next(error);
   }
 
+  const imagePath = place.image
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -196,6 +199,10 @@ const deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+
+  fs.unlink(imagePath, (err)=> {
+    console.log(err)
+  })
 
   res.status(200).json({ message: 'Deleted place.' });
 };
